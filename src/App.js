e164ae901d743eb5a2395980ff7928a021423d73
Ruslan Kultaev <ruslankultaev7@gmail.com>
2023-05-14 17:18:09 +0200

    anecdotes...

--- src/App.js
+++ src/App.js
@@ -1,77 +1,66 @@
 import {useState} from 'react'
 
-const Display = (props) => {
-
-  return (
-    <div>{props.text}</div>
-  )
-}
-
 const Button = (props) => {
-  const {handleClick, text} = props
+
   return (
-    <button onClick={handleClick}>{text}</button>
+      <button onClick={props.clickHandler}> {props.text}</button>
   )
 }
 
-const StatisticsLine = (props) => {
-  return (
-    <table>
-      <tbody>
-        <tr>
-          <td>{props.text}</td>
-          <td>{props.value}</td>
-        </tr>
-      </tbody>
-    </table>
-  )
-}
 
-const Statistics = (props) => {
-    const total = props.good + props.neutral + props.bad
-    const average = 1.0 * (total / 3)
-    const positive = (props.good/total) * 100 + "%"
-  if (props.good > 0 || props.neutral > 0 || props.bad > 0) {
-    return (
-      <div>
-      <StatisticsLine text={"Good "} value={props.good} />
-      <StatisticsLine text={"Neutral "} value={props.neutral} />
-      <StatisticsLine text={"Bad "} value={props.bad} />
-      <StatisticsLine text={"All "} value={total} />
-      <StatisticsLine text={"Average "} value={average} />
-      <StatisticsLine text={"Positive "} value={positive} />
-      </div>
-    )
-  }
-  else {
-    return <p>No feedback given</p>
-  }
-  
+const MostVotes = ({votes, anecdotes}) => {
+  // const [votes, anecdotes] = props
+  const largestVote = Math.max(...votes)
+  const anecdoteIndex = votes.indexOf(largestVote)
+  const topAnecdote = anecdotes[anecdoteIndex]
+  return (
+    <div>
+    <div>{topAnecdote}</div>
+    <div>has {largestVote} votes</div>
+    </div>
+  )
 }
 
 
 const App = () => {
-  const [good, setGood] = useState(0)
-  const [neutral, setNeutral] = useState(0)
-  const [bad, setBad] = useState(0) 
+  const anecdotes = [
+    'If it hurts, do it more often.',
+    'Adding manpower to a late software project makes it later!',
+    'The first 90 percent of the code accounts for the first 10 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
+    'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
+    'Premature optimization is the root of all evil.',
+    'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.',
+    'Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when diagnosing patients.',
+    'The only way to go fast, is to go well.'
+  ]
+  const [selected, setSelected] = useState(0)
+  const [votes, setVotes] = useState(new Array(anecdotes.length).fill(0))
+
+  const selectRandom = () => setSelected(Math.floor(Math.random() * anecdotes.length)) 
+  const handleVotes = () => {
+    const copyVotes = [...votes]
+    copyVotes[selected] += 1
+    setVotes(copyVotes)
+  }
+
 
   return (
-    <>
-      <h1><Display text="give feedback"/></h1>
-      <Button handleClick={() => setGood(good + 1)} text="good"/>
-      <Button handleClick={() => setNeutral(neutral + 1)} text="neutral"/>
-      <Button handleClick={() => setBad(bad + 1)} text="bad"/>
-      <h1><Display text="statistics"/></h1>
+    <div>
       <div>
-        {/* <p>good {good}</p>
-        <p>neutral {neutral}</p>
-        <p>bad {bad}</p> */}
-        <Statistics good={good} neutral={neutral} bad={bad}/>
+        <h1>Anecdote of the day</h1>
+        <p>{anecdotes[selected]}</p>
+        <p>has {votes[selected]} votes</p>
+        <div>
+        <Button text="votes" clickHandler={handleVotes}/>
+        <Button text="next anecdote" clickHandler={selectRandom}/>
+        </div>
       </div>
-
-    </>
-  ) 
+      <div>
+        <h1>Anecdote with most votes</h1>
+        <MostVotes votes={votes} anecdotes={anecdotes}/>
+      </div>
+    </div>
+  )
 }
-
 
 export default App
