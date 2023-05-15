3d8ff6f4fac71b9b08b5b5ca4a134b9f3879754f
Ruslan Kultaev <ruslankultaev7@gmail.com>
2023-05-15 19:03:17 +0200

    testing post and put methods on frontend side

--- src/components/Notes.js
+++ src/components/Notes.js
@@ -1,6 +1,13 @@
 const Notes = (props) => {
+	const label = props.note.important
+		?	'make not important'
+		:	'make important'
+
 	return (
-		<li>{props.note.content}</li>
+		<li>
+			{props.note.content}
+			<button onClick={props.toggleHierarchy}>{label}</button>
+		</li>
 	)
 }
 
